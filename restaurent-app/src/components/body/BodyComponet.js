0d4d6fb3463b1b0988d33c82596d61fb0586dde9
Sojib <alisojib295@gmail.com>
2023-10-29 17:27:40 +0600

    bodyComponet

--- restaurent-app/src/components/body/BodyComponet.js
+++ restaurent-app/src/components/body/BodyComponet.js
@@ -1,9 +1,9 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
 import Home from './Home';
 import Menu from './Menu';
 import Contact from './Contact';
 import About from './About';
-
+import { Route, Redirect , Switch } from "react-router-dom";
 
 
 
@@ -11,13 +11,13 @@ import About from './About';
 const BodyComponet = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="menu" element={<Menu/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/about" element={<About/>} />
-
-      </Routes>
+      <Switch>
+        <Route path="/home"  exact Component={<Home/>} />
+        <Route path="menu" exact Component={<Menu/>} />
+        <Route path="/contact" exact Component={<Contact/>} />
+        <Route path="/about" exact Component={<About/>} />
+        <Redirect from='/' to='/home'/>
+      </Switch>
     </div>
   )
 }
